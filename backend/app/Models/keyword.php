955a754baed756d205ba9aa229f7face1b04fdfd
David de Jesús Martínez Mayeta <97991438+imenvyx@users.noreply.github.com>
2025-10-02 19:51:47 -0400

    refactor: add type hints for BelongsToMany relationships in Task and Keyword models

--- backend/app/Models/keyword.php
+++ backend/app/Models/keyword.php
@@ -2,13 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Keyword extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name'];
 
-    public function tasks()
+    public function tasks(): BelongsToMany
     {
         return $this->belongsToMany(Task::class, 'keyword_task')->withTimestamps();
     }
--- backend/app/Models/task.php
+++ backend/app/Models/task.php
@@ -2,17 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Task extends Model
 {
+    use HasFactory;
     protected $fillable = ['title', 'is_done'];
 
     protected $casts = [
         'is_done' => 'boolean',
     ];
 
-    public function keywords()
+    public function keywords(): BelongsToMany
     {
         return $this->belongsToMany(Keyword::class, 'keyword_task')->withTimestamps();
     }

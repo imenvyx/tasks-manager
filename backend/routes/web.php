a784e940327f661538a611a5bccc7eb9bff208e9
David de Jesús Martínez Mayeta <97991438+imenvyx@users.noreply.github.com>
2025-10-02 20:58:09 -0400

    feat: add routes for managing keywords in web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\KeywordController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('tasks', [TaskController::class, 'index']);
 Route::post('tasks', [TaskController::class, 'store']);
 Route::patch('tasks/{id}/toggle', [TaskController::class, 'toggle']);
+
+// keywords
+Route::get('keywords', [KeywordController::class, 'index']);
+Route::post('keywords', [KeywordController::class, 'store']);
